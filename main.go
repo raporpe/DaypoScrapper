fbc22c386308498afbaf7a97f8a3e26b7cf9710e
raporpe <raporpe@gmail.com>
2020-12-01 17:40:45 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var infoLog = log.New(os.Stdout, "✅ ", log.Lmsgprefix)
+var warningLog = log.New(os.Stdout, "🚨 ", log.Lmsgprefix)
+var errorLog = log.New(os.Stdout, "❌ ", log.Lmsgprefix)
+
+func main() {
+
+
+	idRegex := regexp.MustCompile("id=")
+	poolRegex := regexp.MustCompile("pool=")
+
+	argumentsUse := "Incorrect use of arguments. You must pass the id=number and pool=number arguments"
+
+	var scrapperID string
+	var poolNumber int
+	var idSet bool
+	var poolSet bool
+
+	if len(os.Args) != 3 {
+		errorLog.Println(argumentsUse)
+		return
+	}
+
+	for i, arg := range os.Args {
+
+		// Get the scrapper id
+		if idRegex.MatchString(arg) {
+			scrapperID = strings.ReplaceAll(os.Args[i], "id=", "")
+			idSet = true
+		}
+
+		// Get the scrapper pool size
+		if poolRegex.MatchString(arg) {
+			poolNumber2, err := strconv.Atoi(strings.ReplaceAll(os.Args[i], "pool=", ""))
+			poolNumber = poolNumber2
+			poolSet = true
+
+			if err != nil {
+				errorLog.Println(argumentsUse)
+				return
+			}
+		}
+	}
+
+	if !poolSet || !idSet {
+		errorLog.Println(argumentsUse)
+		return
+	}
+
+	infoLog.Println("Welcome to the Daypo Website Scrapper")
+	infoLog.Println("This scrapper has ID " + scrapperID)
+
+	infoLog.Println("Checking connection to the Daypo.com webpage...")
+	_, err := http.Get("https://www.daypo.com/")
+	if err != nil {
+		errorLog.Fatal("Cannot connect to daypo.com - Closing...")
+		return
+	}
+
+	infoLog.Println("Getting crawl assigments")
+
+	// Get the workload and insert it in the queue
+	urls, err := getStartPage("http://crawler.raporpe.tk/" + scrapperID)
+	scrapperChannel := make(chan string, len(urls))
+	for _, url := range urls {
+		scrapperChannel <- url
+	}
+
+	if err != nil {
+		errorLog.Println("Cannot get crawl assigment. Check your crawl number. -> " + err.Error())
+		return
+	}
+
+
+
+	// Initialize connection to database
+
+
+	infoLog.Println("SPAWNING CRAWLERS -> " + strconv.Itoa(poolNumber))
+
+	dbChannel := make(chan []daypoTest, 1000)
+	// Initialize pool of scrappers
+	for i := 0; i < poolNumber; i++ {
+		go dissectUrl(scrapperChannel, dbChannel)
+	}
+
+	infoLog.Println("SPAWNING DATABSE")
+	//Start database worker
+	go DatabaseWorker(dbChannel)
+
+
+	// Sleep for ever
+	select{}
+
+}
+
+func dissectUrl(scrapperChannel chan string, dbChannel chan []daypoTest) {
+
+	for {
+
+		url := <-scrapperChannel
+
+		if url == "\n" {
+			continue
+		}
+		// Indefinite refresh
+		if url == "main" {
+			for {
+				infoLog.Println("Infinite worker url --> " + url)
+				testsUrl := GetAllDaypoTestUrl("https://daypo.com/")
+				var scrapped []daypoTest
+				scrapped = ScrapDaypoTests(testsUrl)
+				infoLog.Println("Sending results to database")
+				dbChannel <- scrapped
+				time.Sleep(100 * time.Second)
+			}
+		}
+
+		infoLog.Println("Dissecting url --> " + url)
+		testsUrl := GetAllDaypoTestUrl(url)
+		infoLog.Println("Got " + strconv.Itoa(len(testsUrl)) + " suburls in " + url)
+		infoLog.Println("Sending to scrapper")
+
+		var scrapped []daypoTest
+		scrapped = ScrapDaypoTests(testsUrl)
+
+		infoLog.Println("Got " + strconv.Itoa(len(scrapped)) + " tests from " + url)
+		infoLog.Println("Sending results to database")
+
+		dbChannel <- scrapped
+	}
+
+}
+
+
+func workLoadGetter(scrapperChannel chan string) {
+
+
+
+
+
+}
+
+func getStartPage(url string) ([]string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, errors.New("The response was not 200")
+	}
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	res.Body.Close()
+
+	ret := strings.Split(string(content), "\n")
+	return ret[:len(ret)-1], nil
+
+}
+
+func DatabaseWorker(dbChannel chan []daypoTest) {
+
+	infoLog.Println("Connecting to the database...")
+	db, err := sql.Open("mysql", "daypo:daypo2020@tcp(db.raporpe.tk)/daypo")
+	if err != nil {
+		warningLog.Println("There was an error connecting to the database: " + err.Error())
+	}
+	defer db.Close()
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	infoLog.Println("Connected to the database!!!")
+
+	// Infinitely get and insert
+	for {
+
+		batch := <- dbChannel
+
+
+		for _, test := range batch {
+			_, err := db.Exec("INSERT INTO daypo.daypo VALUES (?, ?, ?, ?, ?, ?, ?, ?)", test.title,
+				test.url, test.description, test.date, strconv.Itoa(test.questions), test.author, test.category, test.temary)
+			if err != nil {
+				warningLog.Println("There was an error inserting " + test.url + " into the database: " + err.Error())
+			}
+
+		}
+
+	}
+}
+
---
+++ scrapper.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type daypoTest struct {
+	title       string
+	url string
+	description string
+	date        string
+	questions   int
+	author      string
+	category string
+	temary      string
+}
+
+/*
+Given a daypo url for a test, return a test struct with all the information
+related to that test
+*/
+
+func IsTest(url string) (bool, error) {
+	// Check the given url matches the required standard
+	// STANDARD -> 	/some-test.html or /23423424.html
+	r1, _ := regexp.Compile(`^Test `)
+	r2, _ := regexp.Compile(`.html$`)
+
+	gquery, err := GetGoquery(url)
+	if err != nil {
+		return false, err
+	}
+	title := gquery.Find("title").Text()
+
+	return r1.MatchString(title) && r2.MatchString(url), nil
+
+}
+
+func GetGoquery(url string) (*goquery.Document, error) {
+	req, err := http.Get(url)
+
+	if err != nil {
+		return nil, errors.New("Cannot get webpage: " + err.Error())
+	}
+
+	query, err := goquery.NewDocumentFromReader(req.Body)
+	if err != nil {
+		return nil, errors.New("Fail creating goquery: " + err.Error())
+	}
+	return query, nil
+}
+
+func ScrapDaypoTests(urls []string) []daypoTest  {
+
+	var daypoTests []daypoTest
+
+	for _, url := range urls {
+		isTest, err := IsTest(url)
+		if err != nil {
+			warningLog.Println("There was a goquery error checking if " + url + " is a test: " + err.Error())
+			continue
+		}
+
+		if isTest {
+			gq, err := GetGoquery(url)
+			if err != nil {
+				warningLog.Println("There was a goquery error getting " + url + ": " + err.Error())
+				continue
+			}
+			var daypo daypoTest
+			daypo.title = gq.Find("title").Text()
+			daypo.url = strings.ReplaceAll(url ,"http://daypo.com", "")
+			daypo.author = gq.Find("#ven0 > div.fl.col1.tac > table > tbody > tr > td").Text()
+			daypo.description = gq.Find("#ven0 > div.fl.col1.tac > table > tbody > tr > td").Text()
+			daypo.date = gq.Find("#ven0 > div.fl.col1.tac > table > tbody > tr > td").Text()
+			daypo.temary = gq.Find("#ven0 > div:nth-child(7) > table.w.tal > tbody > tr > td").Text()
+			questions, err := strconv.Atoi(gq.Find("#ven0 > div.fl.col1.tac > table > tbody > tr > td > span:nth-child(25)").Text())
+			if err != nil {
+				warningLog.Println("There was an error getting the number of answers in " + url + ": " + err.Error())
+				questions = 0
+			}
+			daypo.category = gq.Find("#ven0 > div.fl.col1.tac > table > tbody > tr > td > a").Text()
+
+			dateRegex := regexp.MustCompile("[0-9]{2}/[0-9]{2}/[0-9]{4}")
+			daypo.date = dateRegex.FindString(daypo.date)
+
+			descRegex := regexp.MustCompile("Descripción:\n[^\n]*")
+			authorRegex := regexp.MustCompile("Autor:\n[^\n]*")
+
+			daypo.description = descRegex.FindString(daypo.description)
+			daypo.description = strings.ReplaceAll(daypo.description,"Descripción:\n", "")
+
+			daypo.author = authorRegex.FindString(daypo.author)
+			daypo.author = strings.ReplaceAll(daypo.author, "(Otros tests del mismo autor)Fecha de Creación", "")
+			daypo.author = strings.ReplaceAll(daypo.author, "Autor:\n", "")
+			daypo.author = strings.TrimSuffix(daypo.author, ":")
+
+			daypo.questions = questions
+
+			fmt.Println(daypo.url)
+
+			daypoTests = append(daypoTests, daypo)
+		}
+	}
+
+	return daypoTests
+
+
+}
+
+func GetAllDaypoTestUrl(url string) []string {
+
+	var result []string
+	gq, err := GetGoquery(url)
+	if err != nil {
+		warningLog.Println("There was a goquery error extracting suburls in " + url + ": " + err.Error())
+	}
+
+	gq.Find("a[href]").Each(func(index int, item *goquery.Selection) {
+		href, _ := item.Attr("href")
+
+		//Discard urls that do not match standard --> /asdf-adfad.html
+		match, _ := regexp.MatchString("^\\/[a-zA-Z-_0-9]+.html$", href)
+		if match  {
+			href = "http://daypo.com" + href
+			result = append(result, href)
+		}
+	})
+
+	return result
+
+}
